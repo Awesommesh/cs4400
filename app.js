12330f073f50e8ecfcf6ad2157b5c9d2302137c7
Animesh Agrawal <animesh.a.777@gmail.com>
2019-11-28 20:54:06 -0500

    schedule movie and explore movie functional. session cookies added

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var session = require('express-session')({
+  name: 'sid',
+  resave: false,
+  saveUninitialized: false,
+  secret: 'stupidSecret4400', //Session secret
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 2, //TWO HOURS
+    sameSite: true,
+  }
+});
+app.use(session);
 /*
  * As per the instructions, our database is named 'team73'. The server will be hosted
  * on port 3000, and the user/password attributes below should be changed to match your
@@ -54,9 +65,27 @@ for (let i = 0; i < pages.length; i++) {
   });
 }
 
+app.get('/functionality/funcNav', (req, res) => {
+  if (req.session.isCustomer) {
+    if (req.session.isAdmin) {
+      res.redirect('/functionality/admin-customer.html');
+    } else if (req.session.isManager) {
+      res.redirect('/functionality/manager-customer.html');
+    } else {
+      res.redirect('/functionality/customer.html');
+    }
+  } else if (req.session.isAdmin) {
+    res.redirect('/functionality/admin.html');
+  } else if (req.session.isManager) {
+    res.redirect('/functionality/manager.html');
+  } else {
+    res.redirect('/functionality/user.html');
+  }
+});
+
 app.all('*/css/*', (req, res) => {
   let filePath = path.join('./app', req.path);
-  let contentType = fileType(filePath)
+  let contentType = fileType(filePath);
   readFile(filePath, res, contentType);
 });
 
@@ -99,6 +128,7 @@ function fileType(filePath) {
  *   3. Upon invalid login, the app should notify the user, and the user should be allowed to retry.
  */
 app.get('/', (req, res) => {
+  const { username } = req.session;
   let filePath = 'app/templates/index.html';
   let contentType = fileType(filePath);
   readFile(filePath, res, contentType);
@@ -111,7 +141,11 @@ app.post('/login', (req, res) => {
 
   let access = `SELECT * FROM UserLogin`;
   db.query(access, (error, results) => {
-    if (results.length > 0) {
+    if (results.length > 0 && results[0].status == 'Approved') {
+      req.session.username = results[0].username;
+      req.session.isAdmin = results[0].isAdmin;
+      req.session.isManager = results[0].isManager;
+      req.session.isCustomer = results[0].isCustomer;
       if (results[0].isManager == 1 && results[0].isCustomer == 1) {
         res.redirect('functionality/manager-customer.html');
       } else if (results[0].isCustomer == 1) {
@@ -168,6 +202,103 @@ app.post('/theater_overview', (req, res) => {
   let sqlResults = 'SELECT * FROM ManFilterTh';
   db.query(sqlResults, (error, results) => {
     res.redirect('/functionality/theater_overview.html?'+JSON.stringify(results));  //Terrible Terrible way of doing things but what can you do XD
+  });
+});
+
+app.post("/explore_movie", (req, res) => {
+  var {movName, comName, city, state, min_play_date, max_play_date} = req.body;
+  if(min_play_date=='') {
+    min_play_date='1000-01-01';
+  }
+  if(max_play_date=='') {
+    const maxPlayDate = 'SELECT MAX(movPlayDate) AS playDate FROM movieplay';
+    db.query(maxPlayDate, (error, results) => {
+      max_play_date = results[0].playDate;
+    });
+  }
+  var mov_name, com_name;
+  if (movName=="ALL") {
+    mov_name = movName;
+    let sqlGetCompanyName = 'SELECT comName FROM company';
+    db.query(sqlGetCompanyName, (error, results) => {
+      com_name = results[comName].comName;
+      let sql = 'CALL customer_filter_mov(?, ?, ?, ?, ?, ?)';
+      db.query(sql, [mov_name, com_name, city, state, min_play_date, max_play_date], (error, results) => {});
+      let sqlResults = 'SELECT * FROM CosFilterMovie';
+      db.query(sqlResults, (error, results) => {
+        res.redirect('/functionality/explore_movie.html?' + JSON.stringify(results));
+      });
+    });
+  } else {
+    let sqlGetMovieName = 'SELECT movName FROM movie';
+    db.query(sqlGetMovieName, (error, results) => {
+      mov_name = results[movName].movName;
+      let sqlGetCompanyName = 'SELECT comName FROM company';
+      db.query(sqlGetCompanyName, (error, results) => {
+        com_name = results[comName].comName;
+        let sql = 'CALL customer_filter_mov(?, ?, ?, ?, ?, ?)';
+        db.query(sql, [mov_name, com_name, city, state, min_play_date, max_play_date], (error, results) => {});
+        let sqlResults = 'SELECT * FROM CosFilterMovie';
+        db.query(sqlResults, (error, results) => {
+          res.redirect('/functionality/explore_movie.html?' + JSON.stringify(results));
+        });
+      });
+    });
+  }
+});
+
+
+app.get('/movieList/', (req, res)=> {
+  let sql = 'SELECT movName FROM movie';
+  db.query(sql, (error, results)=> {
+    res.json(results);
+  });
+});
+
+app.get('/companyList/', (req, res)=> {
+  let sql = 'SELECT comName FROM company';
+  db.query(sql, (error, results)=> {
+    res.json(results);
+  });
+});
+
+app.get('/creditCardList', (req, res)=> {
+  let sql = "SELECT creditCardNum FROM customercreditcard WHERE username=\""+req.session.username+"\"";
+  db.query(sql, (error, results)=> {
+    res.json(results);
+  });
+});
+
+app.post('/schedule_movie', (req, res) => {
+  var {movName, mov_release_date, mov_play_date} = req.body;
+  let sqlGetMovieName = 'SELECT movName FROM movie';
+  var mov_name;
+  db.query(sqlGetMovieName, (error, results)=> {
+    mov_name = results[movName].movName;
+    let sql = 'CALL manager_schedule_mov(?, ?, ?, ?)';
+    db.query(sql, [req.session.username, mov_name, mov_release_date, mov_play_date], (error, results) => {
+      res.redirect('/functionality/schedule_movie.html');
+    });
+  });
+});
+
+app.post('/view_movie', (req, res) => {
+  var {row, creditCardNum} = req.body;
+  var view_creditCard;
+  let sqlGetCreditCardNum = "SELECT creditCardNum FROM customercreditcard WHERE username=\""+req.session.username+"\"";
+  db.query(sqlGetCreditCardNum, (error, results) => {
+    view_creditCard = results[creditCardNum].creditCardNum;
+  });
+
+  let sqlGetRowData = 'SELECT * FROM CosFilterMovie';
+  db.query(sqlGetRowData, (error, results) => {
+    var row_data= results[row];
+    let sql = 'CALL customer_view_mov(?, ?, ?, ?, ?, ?)';
+    console.log([view_creditCard, row_data.movName, row_data.movReleaseDate, row_data.thName, row_data.comName, row_data.movPlayDate]);
+    db.query(sql, [view_creditCard, row_data.movName, row_data.movReleaseDate, row_data.thName,
+      row_data.comName, row_data.movPlayDate], (error, results)=> {
+      console.log(results);
+    });
   });
 });
 
